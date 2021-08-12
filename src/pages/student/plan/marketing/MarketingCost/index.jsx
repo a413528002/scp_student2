c4ead7d1c60f6f6c4001dbfabaa35e52fe66b777
less is more <18666270412@163.com>
2021-08-12 14:27:41 +0800

    学生：修改精准营销点击提交更新数据

--- src/pages/student/plan/marketing/MarketingCost/index.jsx
+++ src/pages/student/plan/marketing/MarketingCost/index.jsx
@@ -1,104 +1,86 @@
-import React, {useEffect, useState, useRef, useContext} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'umi';
 import {Button, Card, Form, InputNumber} from "antd";
 import PublicTable from "@/components/Table";
 import MarketingCostRule from "@/pages/student/plan/marketing/MarketingCostRule";
 
-const EditableContext = React.createContext(null);
-
-const EditableRow = ({index, ...props}) => {
-  const [form] = Form.useForm();
-  return (
-    <Form form={form} component={false}>
-      <EditableContext.Provider value={form}>
-        <tr {...props} />
-      </EditableContext.Provider>
-    </Form>
-  );
-};
-
 const EditableCell = ({
-                        title,
-                        editable,
-                        children,
-                        dataIndex,
-                        record,
-                        handleSave,
                         editing,
-                        setEditing,
+                        dataIndex,
+                        title,
+                        inputType,
+                        record,
+                        index,
+                        children,
                         ...restProps
                       }) => {
-  // const [editing, setEditing] = useState(false);
-  const inputRef = useRef(null);
-  const form = useContext(EditableContext);
-  console.log(setEditing)
-  useEffect(() => {
-    if (editing) {
-      inputRef.current.focus();
-    }
-  }, [editing]);
-
-  const toggleEdit = () => {
-    setEditing(!editing);
-    form.setFieldsValue({
-      [dataIndex]: record[dataIndex],
-    });
-  };
-
-  const save = async () => {
-    try {
-      const values = await form.validateFields();
-      toggleEdit();
-      handleSave({...record, ...values});
-    } catch (errInfo) {
-      console.log('Save failed:', errInfo);
-    }
-  };
-
-  let childNode = children;
-  // console.log(children)
-  if (editable) {
-    childNode = editing ? (
-      <Form.Item
-        style={{
-          margin: 0,
-        }}
-        name={dataIndex}
-        rules={[
-          {
-            required: true,
-            message: `请输入${title}`,
-          },
-        ]}
-      >
-        <InputNumber
-          ref={inputRef}
-          onPressEnter={save}
-          onBlur={save}
-        />
-      </Form.Item>
-    ) : (
-      <div
-        className="editable-cell-value-wrap"
-        style={{
-          paddingRight: 24,
-        }}
-        onClick={toggleEdit}
-      >
-        {children}
-      </div>
-    );
-  }
-  // console.log(childNode)
-  return <td {...restProps}>{childNode}</td>;
+  const inputNode = <InputNumber min={0}/>;
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `请输入 ${title}`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
 };
 
 
 const MarketingCost = (props) => {
-  const {dispatch, dataSource, loading} = props;
+  const {dispatch, dataSource, loading, submitLoading} = props;
+  const [form] = Form.useForm();
   // 获取课堂id
   const {classHourId} = JSON.parse(localStorage.getItem('STUDENT_IN_CLASS')) || {}
-  const [editing, setEditing] = useState(false);
+  const [data, setData] = useState(dataSource);
+  // 初始编辑row
+  const [editingKey, setEditingKey] = useState(0);
+  const isEditing = (record) => record._key === editingKey;
+  // 保存当前编辑row
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      if (row) {
+        // 投入营销费用
+        dispatch({
+          type: 'studentPlan/inputMarketingCost',
+          payload: {
+            classHourId,
+            ...row
+          }
+        })
+      }
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {...item, ...row});
+        setData(newData);
+        setEditingKey(null);
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey(null);
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
   useEffect(() => {
     if (classHourId) {
       dispatch({
@@ -109,84 +91,59 @@ const MarketingCost = (props) => {
       })
     }
   }, [])
-  console.log()
   const columns = [
     {
       title: '期数',
       dataIndex: 'period',
       key: 'period',
-      // editable: true,
       render: (period) => `第${period}期`
     },
     {
       title: '存款营销费用(万元)',
       dataIndex: 'depositMktCost',
       key: 'depositMktCost',
-      editable: dataSource.every((item=>item.depositMktCost === null)),
       // 当前字段值为null时显示提交按钮 且可以编辑
+      editable: dataSource.every((item => item.depositMktCost === null)),
+      // 表头单位为万元
+      render: (depositMktCost) => `${depositMktCost / 10000}`
     },
     {
       title: '贷款营销费用(万元)',
       dataIndex: 'loanMktCost',
       key: 'loanMktCost',
-      editable: dataSource.every((item=>item.loanMktCost === null)),
       // 当前字段值为null时显示提交按钮 且可以编辑
+      editable: dataSource.every((item => item.loanMktCost === null)),
+      // 表头单位为万元
+      render: (loanMktCost) => `${loanMktCost / 10000}`
     },
     {
       title: '超额补足倍率',
       dataIndex: 'makeUpRate',
       key: 'makeUpRate',
-      // editable: true,
     },
     {
       title: '操作',
       dataIndex: 'operation',
       key: 'operation',
-      render: (_, {depositMktCost, loanMktCost}) => {
+      render: (_, {depositMktCost, loanMktCost, _key}) => {
         return (
           <>
             {
               depositMktCost === null || loanMktCost === null ? (<Button
                 type="primary"
                 size="small"
-                // inputMarketingCost(depositMktCost, loanMktCost)
-                onClick={() => setEditing(true)}
+                loading={submitLoading}
+                onClick={() => save(_key)}
               >
                 提交
-              </Button>) : "不能提交"
+              </Button>) : "已提交"
             }
           </>
         )
       }
     },
   ];
-  // 投入营销费用
-  const inputMarketingCost = (values) => {
-    if (values) {
-      dispatch({
-        type: 'studentPlan/inputMarketingCost',
-        payload: {
-          classHourId,
-          ...values
-        }
-      })
-    }
 
-  }
-  const handleSave = (row) => {
-    // 提交保存的数据
-    inputMarketingCost(row)
-    const newData = [...dataSource];
-    const index = newData.findIndex((item) => row.key === item.key);
-    const item = newData[index];
-    newData.splice(index, 1, {...item, ...row});
-  };
-  const components = {
-    body: {
-      row: EditableRow,
-      cell: EditableCell,
-    },
-  };
   // 表头
   const columnsData = columns.map((col) => {
     if (!col.editable) {
@@ -197,12 +154,10 @@ const MarketingCost = (props) => {
       ...col,
       onCell: (record) => ({
         record,
-        editable: col.editable,
+        inputType: col.dataIndex,
         dataIndex: col.dataIndex,
         title: col.title,
-        handleSave: handleSave,
-        editing: editing,
-        setEditing: setEditing,
+        editing: isEditing(record),
       }),
     };
   });
@@ -212,15 +167,22 @@ const MarketingCost = (props) => {
       bordered={false}
       type='inner'
     >
-      <PublicTable
-        components={components}
-        rowClassName={() => 'editable-row'}
-        dataSource={dataSource}
-        columns={columnsData}
-        loading={loading}
-        bordered
-        pagination={false}
-      />
+      {/*表单提交*/}
+      <Form form={form} component={false}>
+        <PublicTable
+          components={{
+            body: {
+              cell: EditableCell,
+            },
+          }}
+          rowClassName="editable-row"
+          dataSource={dataSource}
+          columns={columnsData}
+          loading={loading}
+          bordered
+          pagination={false}
+        />
+      </Form>
       <MarketingCostRule/>
     </Card>
   );
@@ -228,5 +190,7 @@ const MarketingCost = (props) => {
 
 export default connect(({studentPlan, loading}) => ({
   dataSource: studentPlan.bankMarketingData,
-  loading: loading.effects['studentPlan/queryCurBankMarketing']
+  loading: loading.effects['studentPlan/queryCurBankMarketing'],
+  submitLoading: loading.effects['studentPlan/inputMarketingCost'],
+
 }))(MarketingCost)
