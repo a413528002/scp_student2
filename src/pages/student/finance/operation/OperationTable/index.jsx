d8d9ced1058fcfefdc575f71dbd622dbf2d2e238
less is more <18666270412@163.com>
2021-08-16 17:46:55 +0800

    学生：运营管理

--- src/pages/student/finance/operation/OperationTable/index.jsx
+++ src/pages/student/finance/operation/OperationTable/index.jsx
@@ -1,31 +1,32 @@
-import React, {useState} from 'react';
-import PublicTable from "@/components/Table";
-import {Select, Button, Form, InputNumber} from "antd";
-import styles from '@/pages/student/finance/operation/index.less'
+import React, { useState } from 'react';
+import { connect } from 'umi';
+import PublicTable from '@/components/Table';
+import { Select, Button, Form, InputNumber, Popconfirm, Space } from 'antd';
+import styles from '@/pages/student/finance/operation/index.less';
+
 const originData = [];
 for (let i = 0; i < 8; i++) {
   originData.push({
     _key: i.toString(),
     period: `${i}`,
-    customerTypeName: '假数据',
-    // amount: Math.random() * 100000000,
-    regionName: 'A',
+    typeName: '假数据',
+    amount: null,
+    bankRegion: 'A',
     status: true,
   });
 }
-console.log(originData)
 
 const EditableCell = ({
-                        editing,
-                        dataIndex,
-                        title,
-                        inputType,
-                        record,
-                        index,
-                        children,
-                        ...restProps
-                      }) => {
-  const inputNode = <InputNumber min={0}/>;
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = <InputNumber min={0} />;
   return (
     <td {...restProps}>
       {editing ? (
@@ -37,7 +38,7 @@ const EditableCell = ({
           rules={[
             {
               required: true,
-              message: `请输入 ${title}`,
+              message: `请输入${title}`,
             },
           ]}
         >
@@ -50,45 +51,40 @@ const EditableCell = ({
   );
 };
 
-const OperationTable = () => {
-  // const dataSource = [];
+const OperationTable = (props) => {
+  const { dispatch } = props;
   const [form] = Form.useForm();
-  // 获取课堂id
-  const {classHourId} = JSON.parse(localStorage.getItem('STUDENT_IN_CLASS')) || {}
-  const [data, setData] = useState(originData);
-  // 初始编辑row
-  const [editingKey, setEditingKey] = useState(["0","1","2","3"]);
-  // editingKey.map((item)=>)
+  // 当前编辑row key
+  const [editingKey, setEditingKey] = useState('');
+  // 是否可编辑
   const isEditing = (record) => record._key === editingKey;
-  // const isEditing = (record) => true;
-  // 保存当前编辑row
-  const save = async (key) => {
-    try {
-      const row = await form.validateFields();
-      console.log(row)
-      /*if (row) {
-        // 投入营销费用
-        dispatch({
-          type: 'studentPlan/inputMarketingCost',
-          payload: {
-            classHourId,
-            ...row
-          }
-        })
-      }*/
-      const newData = [...data];
-      const index = newData.findIndex((item) => key === item.key);
+  // 编辑
+  const handleEdit = (record) => {
+    form.setFieldsValue({
+      ...record,
+    });
+    setEditingKey(record._key);
+  };
+  // 取消编辑pop
+  const handleCancelPop = () => {
+    setEditingKey('');
+  };
 
-      if (index > -1) {
-        const item = newData[index];
-        newData.splice(index, 1, {...item, ...row});
-        setData(newData);
-        setEditingKey(null);
-      } else {
-        newData.push(row);
-        setData(newData);
-        setEditingKey(null);
-      }
+  const updateBankExpense = async (bankExpenseId) => {
+    try {
+      const amount = await form.validateFields();
+      console.log(row);
+      console.log(key);
+      /*dispatch({
+        type: '',
+        payload: {
+          classHourId,
+          bankExpenseId,
+          amount,
+        },
+      });*/
+
+      setEditingKey('');
     } catch (errInfo) {
       console.log('Validate Failed:', errInfo);
     }
@@ -98,28 +94,57 @@ const OperationTable = () => {
       title: '期数',
       dataIndex: 'period',
       key: 'period',
-      render:(period)=>`第${period}期`
+      render: (period) => `第${period}期`,
     },
     {
       title: '类别',
-      dataIndex: 'age',
-      key: 'age',
+      dataIndex: 'typeName',
+      key: 'typeName',
     },
     {
       title: '总行/支行',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'bankTypeName',
+      key: 'bankTypeName',
     },
     {
       title: '区域',
-      dataIndex: 'address',
-      key: 'address',
+      dataIndex: 'bankRegion',
+      key: 'bankRegion',
     },
     {
       title: '费用(万元)',
+      dataIndex: 'amount',
+      key: 'amount',
+      editable: true,
+    },
+    {
+      title: '操作',
       dataIndex: '_key',
       key: '_key',
-      editable: true
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <Space>
+            <Button type="primary" size="small" onClick={() => updateBankExpense(record._key)}>
+              保存
+            </Button>
+            <Popconfirm title="确认取消?" onConfirm={handleCancelPop}>
+              <Button type="link" size="small">
+                取消
+              </Button>
+            </Popconfirm>
+          </Space>
+        ) : (
+          <Button
+            type="link"
+            size="small"
+            disabled={editingKey !== ''}
+            onClick={() => handleEdit(record)}
+          >
+            编辑
+          </Button>
+        );
+      },
     },
   ];
   // 表头
@@ -142,11 +167,10 @@ const OperationTable = () => {
   return (
     <>
       <div className={styles.choose}>
-        <Select defaultValue="one" style={{width: 120}}>
+        <Select defaultValue="one" style={{ width: 120 }}>
           <Select.Option value="one">第一期</Select.Option>
           <Select.Option value="two">第二期</Select.Option>
         </Select>
-        <Button type="primary" onClick={()=>save()}>保存</Button>
       </div>
       {/*表单提交*/}
       <Form form={form} component={false}>
@@ -166,4 +190,4 @@ const OperationTable = () => {
   );
 };
 
-export default OperationTable;
+export default connect()(OperationTable);
