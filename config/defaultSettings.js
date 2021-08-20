ca6e2ae20f4574bb60a700c849947b699f4619c0
less is more <18666270412@163.com>
2021-08-20 09:05:14 +0800

    修改了主题色

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -1,13 +1,14 @@
 const Settings = {
   navTheme: 'light',
   // 拂晓蓝
-  primaryColor: '#1890ff',
+  // primaryColor: '#1890ff',
+  primaryColor: 'red',
   layout: 'mix',
   contentWidth: 'Fluid',
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: '奢侈品',
+  title: '商业银行经营管理实训平台',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
