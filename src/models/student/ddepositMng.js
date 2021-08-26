1eb3bf1324c410780da30d95193791c31cf794db
less is more <18666270412@163.com>
2021-08-26 14:55:12 +0800

    修复利息丢失代码

--- src/models/student/ddepositMng.js
+++ src/models/student/ddepositMng.js
@@ -1,5 +1,5 @@
-import {message} from "antd";
-import {queryDepositInterests, queryDeposits, updateDepositInterest} from "@/services/student/dm";
+import { message } from 'antd';
+import { queryDepositInterests, queryDeposits, updateDepositInterest } from '@/services/student/dm';
 
 // 存款管理
 const DepositsMngModel = {
@@ -7,62 +7,68 @@ const DepositsMngModel = {
   state: {
     queryDepositsData: [],
     queryDepositInterestsData: [],
+    editBankFinancialBusinessInstId: undefined,
   },
   effects: {
-
     // 查询存款
-    * queryDeposits({payload}, {call, put,}) {
-      const response = yield call(queryDeposits, payload)
+    *queryDeposits({ payload }, { call, put }) {
+      const response = yield call(queryDeposits, payload);
       if (!response.errCode) {
         const queryDepositsData = response.map((item, index) => {
           return {
             ...item,
-            _key: item.bankFinancialBusinessId
-          }
+            _key: item.bankFinancialBusinessId,
+          };
         });
         yield put({
           type: 'save',
           payload: {
-            queryDepositsData
-          }
-        })
+            queryDepositsData,
+          },
+        });
       }
     },
 
     // 查询存款利息
-    * queryDepositInterests({payload, callback}, {call, put}) {
-      const response = yield call(queryDepositInterests, payload)
+    *queryDepositInterests({ payload, callback }, { call, put }) {
+      const response = yield call(queryDepositInterests, payload);
       if (!response.errCode) {
-        const queryDepositInterestsData = response.map((item, index) => {
+        // 获取可以编辑的bankFinancialBusinessInstId
+        const editBankFinancialBusinessInstId = response
+          .filter((item) => item._disable === false)
+          .map((item) => item.bankFinancialBusinessInstId)
+          .toString();
+        const queryDepositInterestsData = response.map((item) => {
           return {
             ...item,
-            _key: index
-          }
+            _key: item.bankFinancialBusinessInstId,
+          };
         });
         yield put({
           type: 'save',
           payload: {
-            queryDepositInterestsData
-          }
-        })
+            queryDepositInterestsData,
+            editBankFinancialBusinessInstId,
+          },
+        });
       }
     },
     // 查询存款利息
-    * updateDepositInterest({payload, callback}, {call, put}) {
-      const response = yield call(updateDepositInterest, payload)
+    *updateDepositInterest({ payload, callback }, { call, put }) {
+      const response = yield call(updateDepositInterest, payload);
       if (!response.errCode) {
-        message.success('保存成功')
-        callback()
+        message.success('保存成功');
+        callback();
       }
     },
   },
   reducers: {
-    save(state, {payload}) {
+    save(state, { payload }) {
       return {
         ...state,
         ...payload,
-      }
-    }
-  }
-}
-export default DepositsMngModel
+      };
+    },
+  },
+};
+export default DepositsMngModel;
--- src/models/student/dloanMng.js
+++ src/models/student/dloanMng.js
@@ -1,69 +1,75 @@
-import {message} from "antd";
+import { message } from 'antd';
 
-import {queryLoanInterests, queryLoans, updateLoanInterest} from "@/services/student/lm";
+import { queryLoanInterests, queryLoans, updateLoanInterest } from '@/services/student/lm';
 
 // 贷款管理
 const LoanMngModel = {
   namespace: 'studentLoanMng',
   state: {
     queryLoansData: [],
-    queryLoanInterestsData: []
+    queryLoanInterestsData: [],
+    editBankFinancialBusinessInstId: undefined,
   },
   effects: {
-
     // 查询贷款
-    * queryLoans({payload}, {call, put,}) {
-      const response = yield call(queryLoans, payload)
+    *queryLoans({ payload }, { call, put }) {
+      const response = yield call(queryLoans, payload);
       if (!response.errCode) {
         const queryLoansData = response.map((item) => {
           return {
             ...item,
-            _key: item.bankFinancialBusinessId
-          }
+            _key: item.bankFinancialBusinessId,
+          };
         });
         yield put({
           type: 'save',
           payload: {
-            queryLoansData
-          }
-        })
+            queryLoansData,
+          },
+        });
       }
     },
 
     // 查询贷款利息
-    * queryLoanInterests({payload, callback}, {call, put}) {
-      const response = yield call(queryLoanInterests, payload)
+    *queryLoanInterests({ payload, callback }, { call, put }) {
+      const response = yield call(queryLoanInterests, payload);
       if (!response.errCode) {
-        const queryLoanInterestsData = response.map((item, index) => {
+        // 获取可以编辑的bankFinancialBusinessInstId
+        const editBankFinancialBusinessInstId = response
+          .filter((item) => item._disable === false)
+          .map((item) => item.bankFinancialBusinessInstId)
+          .toString();
+        const queryLoanInterestsData = response.map((item) => {
           return {
             ...item,
-            _key: index
-          }
+            _key: item.bankFinancialBusinessInstId,
+          };
         });
         yield put({
           type: 'save',
           payload: {
-            queryLoanInterestsData
-          }
-        })
+            queryLoanInterestsData,
+            editBankFinancialBusinessInstId,
+          },
+        });
       }
     },
     // 保存存款利息
-    * updateLoanInterest({payload, callback}, {call, put}) {
-      const response = yield call(updateLoanInterest, payload)
+    *updateLoanInterest({ payload, callback }, { call, put }) {
+      const response = yield call(updateLoanInterest, payload);
       if (!response.errCode) {
-        message.success('保存成功')
-        callback()
+        message.success('保存成功');
+        callback();
       }
     },
   },
   reducers: {
-    save(state, {payload}) {
+    save(state, { payload }) {
       return {
         ...state,
         ...payload,
-      }
-    }
-  }
-}
-export default LoanMngModel
+      };
+    },
+  },
+};
+export default LoanMngModel;
