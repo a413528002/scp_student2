6f2f1041b1cd6bec875a85861a9ea8312f8e4135
less is more <18666270412@163.com>
2021-09-24 11:58:54 +0800

    老师端：课堂信息，开始上课改为独立按钮操作

--- src/models/teacher/classroom.js
+++ src/models/teacher/classroom.js
@@ -17,132 +17,139 @@ const ClassroomModel = {
   },
   effects: {
     // 选择课堂
-    * switchClassroom({payload}, {call, put}) {
+    *switchClassroom({ payload }, { put }) {
+      console.log(payload);
       if (!payload) {
-        return
+        return;
       }
-      const {classHourId} = payload
+      const { classHourId } = payload;
       if (!classHourId) {
-        return
+        return;
       }
-      localStorage.setItem('TEACHER_IN_CLASS', JSON.stringify(payload))
+      // 存储到 localStorage
+      localStorage.setItem('TEACHER_IN_CLASS', JSON.stringify(payload));
+      // 存储 redux
       yield put({
         type: 'save',
         payload: {
           teacherClassroomTeacherInClassData: payload,
-        }
-      })
+        },
+      });
+      // 查询课堂成员
       yield put({
         type: 'queryClassHourUsers',
-        payload: {classHourId}
-      })
+        payload: { classHourId },
+      });
     },
     // 创建课堂
-    * createClassHour({payload, callback}, {call, put}) {
-      const response = yield call(getTeacherCreateClassHour, payload)
+    *createClassHour({ payload, callback }, { call, put }) {
+      const response = yield call(getTeacherCreateClassHour, payload);
       if (!response.errCode) {
-        message.success('新建成功')
-        callback()
+        message.success('新建成功');
+        callback();
         // 同时存储一份在redux中
         yield put({
           type: 'save',
           payload: {
-            teacherClassroomTeacherInClassData: response
-          }
-        })
+            teacherClassroomTeacherInClassData: response,
+          },
+        });
         yield put({
           type: 'switchClassroom',
-          payload: response
-        })
+          payload: response,
+        });
       }
     },
     // 查询我管理的课堂
-    * queryMyClassHours({payload}, {call, put}) {
-      const response = yield call(getTeacherQueryMyClassHours, payload)
+    *queryMyClassHours({ payload }, { call, put }) {
+      const response = yield call(getTeacherQueryMyClassHours, payload);
       if (!response.errCode) {
-        response.content.map(item => item._key = item.classHourId)
+        response.content.map((item) => (item._key = item.classHourId));
         yield put({
           type: 'save',
           payload: {
             teacherClassroomQueryMyClassHoursData: response,
-          }
-        })
+          },
+        });
       }
     },
     // 开始课堂
-    * startClassHour({payload, callback}, {call, put}) {
-      const response = yield call(getTeacherStartClassHour, payload)
+    *startClassHour({ payload }, { call, put }) {
+      const response = yield call(getTeacherStartClassHour, payload);
       if (!response.errCode) {
-        message.success('切换课堂成功')
+        message.success('操作成功');
         // 同时存储一份在redux中
         yield put({
           type: 'save',
           payload: {
-            teacherClassroomTeacherInClassData: response
-          }
-        })
-        // 切换课程成功的回调 用于关闭modal
-        callback(response)
+            teacherClassroomTeacherInClassData: response,
+          },
+        });
+
+        yield put({
+          type: 'switchClassroom',
+          payload: { ...response },
+        });
       }
     },
 
     // 结束课堂
-    * endClassHour({payload, callback}, {call, put}) {
-      const response = yield call(getTeacherEndClassHour, payload)
+    *endClassHour({ payload, callback }, { call, put }) {
+      const response = yield call(getTeacherEndClassHour, payload);
       if (!response.errCode) {
-        message.success('已结束当前课堂')
+        message.success('已结束当前课堂');
         // redux也赋值为空undefined
         yield put({
           type: 'save',
           payload: {
-            teacherClassroomTeacherInClassData: {}
-          }
-        })
+            teacherClassroomTeacherInClassData: {},
+          },
+        });
       }
     },
     // 查询课堂成员
-    * queryClassHourUsers({payload}, {call, put}) {
-      const response = yield call(getTeacherQueryClassHourUsers, payload)
+    *queryClassHourUsers({ payload }, { call, put }) {
+      const response = yield call(getTeacherQueryClassHourUsers, payload);
       if (!response.errCode) {
-        response.map(item => item._key = item.classHourUserId)
+        response.map((item) => (item._key = item.classHourUserId));
         yield put({
           type: 'save',
           payload: {
-            teacherClassroomQueryClassHourUsersData: response
-          }
-        })
+            teacherClassroomQueryClassHourUsersData: response,
+          },
+        });
       }
     },
     // 踢出课堂成员
-    * kickClassHourUser({payload}, {call, put}) {
-      const response = yield call(getTeacherKickClassHourUser, payload)
+    *kickClassHourUser({ payload }, { call, put }) {
+      const response = yield call(getTeacherKickClassHourUser, payload);
       if (!response.errCode) {
         // 刷新表格
         // 获取课堂id
-        const {classHourId} = payload
+        const { classHourId } = payload;
         yield put({
           type: 'queryClassHourUsers',
-          payload: {classHourId}
-        })
-        message.success('已踢出')
+          payload: { classHourId },
+        });
+        message.success('已踢出');
       }
     },
   },
   reducers: {
-    save(state, {payload}) {
+    save(state, { payload }) {
       return {
         ...state,
         ...payload,
-      }
+      };
     },
     reset() {
       return {
         teacherClassroomTeacherInClassData: {},
         teacherClassroomQueryMyClassHoursData: {},
         teacherClassroomQueryClassHourUsersData: [],
-      }
-    }
+      };
+    },
   },
-}
+};
 
-export default ClassroomModel
+export default ClassroomModel;
