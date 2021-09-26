aa6f43119b990aa0fec6e465f9a942f634c2f1d6
less is more <18666270412@163.com>
2021-09-26 19:42:28 +0800

    老师端：添加存款记录、贷款记录

--- src/pages/teacher/business/BusinessTabs/index.jsx
+++ src/pages/teacher/business/BusinessTabs/index.jsx
@@ -4,6 +4,8 @@ import SubmitContent from '@/pages/teacher/business/SubmitContent';
 import InstitutionsContent from '@/pages/teacher/business/InstitutionsContent';
 import ErrorContent from '@/pages/teacher/business/ErrorContent';
 import MarketingContent from '@/pages/teacher/business/MarketingContent';
+import DepositContent from '@/pages/teacher/business/depositContent';
+import LoanContent from '@/pages/teacher/business/loanContent';
 
 const { TabPane } = Tabs;
 const BusinessTabs = () => {
@@ -16,32 +18,40 @@ const BusinessTabs = () => {
     {
       key: 'submit',
       tab: '提交记录',
+      content: <SubmitContent />,
     },
     {
       key: 'marketing',
       tab: '营销费用',
+      content: <MarketingContent />,
     },
     {
       key: 'institutions',
       tab: '机构建设',
+      content: <InstitutionsContent />,
+    },
+    {
+      key: 'deposit',
+      tab: '存款记录',
+      content: <DepositContent />,
+    },
+    {
+      key: 'loan',
+      tab: '贷款记录',
+      content: <LoanContent />,
     },
     {
       key: 'error',
       tab: '错误记录',
+      content: <ErrorContent />,
     },
   ];
 
-  const contentList = {
-    submit: <SubmitContent />,
-    marketing: <MarketingContent />,
-    institutions: <InstitutionsContent />,
-    error: <ErrorContent />,
-  };
   return (
     <Tabs onChange={callback}>
-      {tabList.map(({ tab, key }) => (
+      {tabList.map(({ tab, key, content }) => (
         <TabPane tab={tab} key={key}>
-          {contentList[key]}
+          {content}
         </TabPane>
       ))}
     </Tabs>
---
+++ src/pages/teacher/business/loanContent/index.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import { connect } from 'umi';
+import PublicTable from '@/components/Table';
+import Tags from "@/components/Tags";
+import Million from "@/components/Million";
+import {toPercent} from "@/utils/commonUtils";
+
+const LoanContent = (props) => {
+  const { dispatch, loading } = props;
+  const { dataSource } = props;
+  // 获取课堂id
+  const { classHourId } = JSON.parse(localStorage.getItem('TEACHER_IN_CLASS')) || {};
+  useEffect(() => {
+    // 查询各个银行机构信息
+    if (classHourId) {
+      /* dispatch({
+        type: 'teacherBusiness/queryBankOrganizations',
+        payload: { classHourId },
+      }); */
+    }
+  }, []);
+  const columns = [
+    {
+      title: '所属期数',
+      dataIndex: 'period',
+      render: (period) => `第${period}期`,
+    },
+    {
+      title: '业务类型',
+      dataIndex: 'loanTypeName',
+      render: (loanTypeName) => <Tags>{loanTypeName}</Tags>,
+    },
+    {
+      title: '金额(万元)',
+      dataIndex: 'amount',
+      render: (amount) => <Million>{amount}</Million>,
+    },
+    {
+      title: '利率',
+      dataIndex: 'expectRate',
+      render: (val) => toPercent(val),
+    },
+    {
+      title: '期限',
+      dataIndex: 'term',
+    },
+    {
+      title: '贷款分类',
+      dataIndex: 'creditRating',
+    },
+    {
+      title: '利率类型',
+      dataIndex: 'rateTypeName',
+      render: (rateTypeName) => <Tags>{rateTypeName}</Tags>,
+    },
+    {
+      title: '质押/担保金额(万元)',
+      dataIndex: 'mgMoney',
+      render: (amount) => <Million>{amount}</Million>,
+    },
+    {
+      title: '渠道类型',
+      dataIndex: 'channelName',
+    },
+    {
+      title: '区域',
+      dataIndex: 'regionName',
+    },
+  ];
+  return <PublicTable dataSource={dataSource} columns={columns} loading={loading} bordered />;
+};
+
+export default connect(({ teacherBusiness, loading }) => ({
+  // dataSource: teacherBusiness.queryBankOrganizationsData,
+  loading: loading.effects['teacherBusiness/queryBankOrganizations'],
+}))(LoanContent);
