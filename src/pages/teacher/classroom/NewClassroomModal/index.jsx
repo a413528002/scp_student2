374a073380905f73bcf1cd7c3117ccab8905d5e9
less is more <18666270412@163.com>
2021-10-09 16:02:04 +0800

    新建课堂 添加loading

--- src/pages/teacher/classroom/NewClassroomModal/index.jsx
+++ src/pages/teacher/classroom/NewClassroomModal/index.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
-import {connect} from "umi";
-import {Modal, Form, Input, Button, Space} from 'antd';
+import { connect } from 'umi';
+import { Modal, Form, Input, Button, Space } from 'antd';
 
 const NewClassroomModal = (props) => {
-  const {newClassroomModalVisible, handleNewClassroomCancelModal, dispatch} = props
+  const { newClassroomModalVisible, handleNewClassroomCancelModal } = props;
+  const { createLoading, dispatch } = props;
   const [form] = Form.useForm();
   // 新建课堂
   const createClassHour = (params) => {
     dispatch({
       type: 'teacherClassroom/createClassHour',
-      payload: {...params},
+      payload: { ...params },
       // 新建成功后的回调
       callback: () => {
-        handleCancelResetFields()
+        handleCancelResetFields();
       },
-    })
-  }
+    });
+  };
   /**
    * 提交表单 调用新建课堂
    * @param values 表单字段值
    */
   const onFinish = (values) => {
-    createClassHour(values)
-  }
+    createClassHour(values);
+  };
   // 关闭modal 重置表单
   const handleCancelResetFields = () => {
     // 关闭modal
-    handleNewClassroomCancelModal()
+    handleNewClassroomCancelModal();
     // 重置表单
-    form.resetFields()
-  }
+    form.resetFields();
+  };
   return (
     <Modal
       visible={newClassroomModalVisible}
@@ -38,12 +39,7 @@ const NewClassroomModal = (props) => {
       footer={null}
       width={320}
     >
-      <Form
-        form={form}
-        layout="vertical"
-        name="NewClassroomModal"
-        onFinish={onFinish}
-      >
+      <Form form={form} layout="vertical" name="NewClassroomModal" onFinish={onFinish}>
         <Form.Item
           name="classHourName"
           rules={[
@@ -53,14 +49,14 @@ const NewClassroomModal = (props) => {
             },
           ]}
         >
-          <Input placeholder='请输入课堂名称'/>
+          <Input placeholder="请输入课堂名称" />
         </Form.Item>
-        <Form.Item style={{textAlign: "center", marginBottom: 0}}>
+        <Form.Item style={{ textAlign: 'center', marginBottom: 0 }}>
           <Space>
             <Button htmlType="button" onClick={handleCancelResetFields}>
               取消
             </Button>
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" htmlType="submit" loading={createLoading}>
               确认
             </Button>
           </Space>
@@ -70,4 +66,6 @@ const NewClassroomModal = (props) => {
   );
 };
 
-export default connect()(NewClassroomModal);
+export default connect(({ loading }) => ({
+  createLoading: loading.effects['teacherClassroom/createClassHour'],
+}))(NewClassroomModal);
